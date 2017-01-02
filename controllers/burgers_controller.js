1c4aa2116aeba7aa06d2cc4863ba9840f5d0f1f2
ShannonGreen27 <shannon.jm.green@gmail.com>
2017-01-01 22:34:46 -0600

    removed customer orm and added sequelize

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,32 +3,44 @@ Here is where you create all the functions that will do the routing for your app
 */
 var express = require('express');
 var router = express.Router();
-var burger = require('../models/burger.js');
+var models = require('../models');
 
 router.get('/', function (req, res) {
 	res.redirect('/burgers');
 });
 
 router.get('/burgers', function (req, res) {
-	burger.all(function (data) {
-		var hbsObject = { burgers: data };
-		console.log(hbsObject);
-		res.render('index', hbsObject);
+	models.Burger.findAll({
+
+	})
+	.then(function (burgers) {
+		res.render('index', {
+			burgers: burgers
+		});
 	});
 });
 
 router.post('/burgers/create', function (req, res) {
-	burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function() {
+	models.Burger.create({
+		burger_name: req.body.burger_name, 
+		devoured: req.body.devoured
+	})
+	.then(function() {
 		res.redirect('/burgers');
 	});
 });
 
 router.put('/burgers/update/:id', function (req, res) {
-	var condition = 'id = ' + req.params.id;
 
-	console.log('condition', condition);
-
-	burger.update({ devoured: req.body.devoured }, condition, function () {
+	models.Burger.update({
+		devoured: req.body.devoured
+	},
+	{
+		where: { 
+			id: req.params.id
+		}
+	})
+	.then(function(result) {
 		res.redirect('/burgers');
 	});
 });
--- server.js
+++ server.js
@@ -2,12 +2,28 @@
 Here is where you set up your server file.
 express middleware.
 */
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+// Our model controllers (rather than routes)
+var routes = require('./controllers/burgers_controller.js');
+
+// we bring in the models we exported with index.js
+var models = require("./models");
+
+// instantiate our app
 var app = express();
+
+//set up handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// override POST to have DELETE and PUT
+app.use(methodOverride('_method'));
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
@@ -16,16 +32,17 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
-
-var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
+// we set the port of the app
+app.set('port', process.env.PORT || 3000);
+
+// we sync the models with our db 
+// (thus creating the apropos tables)
+models.sequelize.sync().then(function () {
+	// set our app to listen to the port we set above
+  var server = app.listen(app.get('port'), function() {
+  	// then display the listening port.
+    console.log('Express server listening on port ' + server.address().port);
+  });
+});
